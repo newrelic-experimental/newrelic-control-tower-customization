d623b9398b4315b3ad2797699e7ac22dac4203f6
wellsiau.aws <wellsiau@amazon.com>
2021-02-05 21:49:12 +0000

    Add zip copy function

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+QSS3BucketName=$1
+QSS3KeyPrefix=$2
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pushd $SCRIPT_DIRECTORY > /dev/null
+cd src
+zip -r newrelic_ct_customization.zip .
+popd > /dev/null
+aws s3 cp ./src/newrelic_ct_customization.zip  s3://$QSS3BucketName/$QSS3KeyPrefix/newrelic_ct_customization.zip --acl public-read
+aws s3 cp ./templates/newrelic_ct_customization.yaml s3://$QSS3BucketName/$QSS3KeyPrefix/newrelic_ct_customization.yaml --acl public-read
