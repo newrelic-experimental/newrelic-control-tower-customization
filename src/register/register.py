874d572e14661520196d278fdd940d2ef5d62293
wellsiau.aws <wellsiau@amazon.com>
2021-02-11 03:27:13 +0000

    graphql integration logic

--- src/register/register.py
+++ src/register/register.py
@@ -58,7 +58,24 @@ def stackset_check(messages):
                 
                 elif stackset_status['StackSetOperation']['Status'] in ['SUCCEEDED']:
                     logger.info("Start registration")
-                    newrelic_registration(stackset_status)
+                    cloudFormationPaginator = cloudFormationClient.get_paginator('list_stack_set_operation_results')
+                    stackset_iterator = cloudFormationPaginator.paginate(
+                        StackSetName=stackSetName,
+                        OperationId=params['OperationId']
+                    )
+                    
+                    newRelicSecret = os.environ['newRelicSecret']
+                    newRelicAccId = os.environ['newRelicAccId']
+                    newRelicAccessKey = get_secret_value(newRelicSecret)
+                    newRelicIntegrationList = newrelic_get_schema(newRelicAccessKey)
+                    
+                    if newRelicAccessKey:
+                        for page in stackset_iterator:
+                            if 'Summaries' in page:
+                                for operation in page['Summaries']:
+                                    if operation['Status'] in ('SUCCEEDED'):
+                                        targetAccount = operation['Account']
+                                        newrelic_registration(targetAccount, newRelicAccessKey, newRelicAccId, newRelicIntegrationList)
                     
                 elif stackset_status['StackSetOperation']['Status'] in ['FAILED','STOPPED']:
                     logger.warning("Stackset operation failed/stopped")
@@ -75,21 +92,162 @@ def stackset_check(messages):
         except Exception as e:
             logger.error(e)
 
-def newrelic_registration(stackset_status):
-    newRelicSecret = os.environ['newRelicSecret']
+def get_secret_value(secret_arn):
     secretClient = session.client('secretsmanager')
-    
     try:
         secret_response = secretClient.get_secret_value(
-            SecretId=newRelicSecret
+            SecretId=secret_arn
         )
-    except Exception as e:
-        logger.error("Get Secret Failed: " + str(e))
-    else:
         if 'SecretString' in secret_response:
-            secret = json.loads(secret_response['SecretString'])
-            logger.info(secret['AccessKey'])
+            secret = json.loads(secret_response['SecretString'])['AccessKey']
+            return secret 
     
+    except Exception as e:
+        logger.error('Get Secret Failed: ' + str(e))
+    
+def newrelic_registration(aws_account_id, access_key, newrelic_account_id, newrelic_integration_list):
+    role_arn =  'arn:aws:iam::{}:role/NewRelicIntegrationRole_{}'.format(aws_account_id, newrelic_account_id)
+    nerdGraphEndPoint = os.environ['nerdGraphEndPoint']
+    
+    link_payload = '''
+    mutation 
+    {{
+        cloudLinkAccount(accountId: {0}, accounts: 
+        {{
+            aws: [
+            {{
+                name: "{1}", 
+                arn: "{2}"
+            }}]
+        }}) 
+        {{
+            linkedAccounts 
+            {{
+                id name authLabel
+            }}
+            errors 
+            {{
+                type
+                message
+            }}
+        }}
+    }}
+    '''.format(newrelic_account_id, aws_account_id, role_arn)
+    logger.info('NerdGraph link account payload : {}'.format(json.dumps(link_payload)))
+    
+    response = requests.post(nerdGraphEndPoint, headers={'API-Key': access_key}, verify=True, data=link_payload)
+    logger.info('NerdGraph response code : {}'.format(response.status_code))
+    logger.info('NerdGraph response : {}'.format(json.dumps(response.text)))
+    if response.status_code == 200:
+        link_response = json.loads(response.text)
+        link_accound_id = link_response['data']['cloudLinkAccount']['linkedAccounts'][0]['id']
+        if len(link_response['data']['cloudLinkAccount']['errors']) > 10:
+            logger.warning('NerdGraph error messages : {}'.format(link_response['data']['cloudLinkAccount']['errors']))
+        else:
+            service_payload = []
+            for service in newrelic_integration_list:
+                service_payload.append('{0}: [{{ linkedAccountId: {1} }}]'.format(service, link_accound_id))
+            
+            integration_payload = '''
+            mutation 
+            {{
+              cloudConfigureIntegration (
+                accountId: {0},
+                integrations: 
+                {{
+                  aws: 
+                  {{
+                    {1}
+                  }}
+                }} 
+              ) 
+              {{
+                integrations 
+                {{
+                  id
+                  name
+                  service 
+                  {{
+                    id 
+                    name
+                  }}
+                }}
+                errors 
+                {{
+                  type
+                  message
+                }}
+              }}
+            }}
+            '''.format(newrelic_account_id, '\n'.join(service_payload[1:]))
+            logger.info('NerdGraph integration payload : {}'.format(json.dumps(integration_payload)))
+            integration_response = requests.post(nerdGraphEndPoint, headers={'API-Key': access_key}, verify=True, data=integration_payload)
+            logger.info('NerdGraph integration response code : {}'.format(integration_response.status_code))
+            logger.info('NerdGraph integration response : {}'.format(integration_response.text))
+ 
+def newrelic_get_integration(access_key, newrelic_account_id):
+    nerdGraphEndPoint = os.environ['nerdGraphEndPoint']
+    service_query = '''
+    {{
+      actor 
+      {{
+        account(id: {0}) 
+        {{
+          cloud 
+          {{
+            provider(slug: "aws") 
+            {{
+              services 
+              {{
+                slug
+              }}
+            }}
+          }}
+        }}
+      }}
+    }}
+    '''.format(newrelic_account_id)
+    
+    try:
+        response = requests.post(nerdGraphEndPoint, headers={'API-Key': access_key}, verify=True, data=service_query)
+        temp_list = json.loads(response.text)['data']['actor']['account']['cloud']['provider']['services']
+        newrelic_integration_list = []
+        for slug in temp_list:
+            newrelic_integration_list.append(slug['slug'])
+        logger.info('NerdGraph AWS available integrations : {}'.format(newrelic_integration_list))
+        return newrelic_integration_list
+    except Exception as e:
+        logger.error(e)
+
+def newrelic_get_schema(access_key):
+    nerdGraphEndPoint = os.environ['nerdGraphEndPoint']
+    schema_query = '''
+    {
+        __schema 
+        {
+            types 
+            {
+                name
+                inputFields 
+                {
+                    name
+                }
+            }
+        }
+    }
+    '''
+    try:
+        response = requests.post(nerdGraphEndPoint, headers={'API-Key': access_key}, verify=True, data=schema_query)
+        logger.info(json.loads(response.text))
+        temp_types = json.loads(response.text)['data']['__schema']['types']
+        temp_integration_input = [input['inputFields'] for input in temp_types if input['name'] == 'CloudAwsIntegrationsInput'][0]
+        newrelic_integration_list = [input['name'] for input in temp_integration_input]
+        logger.info('NerdGraph AWS available integrations : {}'.format(newrelic_integration_list))
+        return newrelic_integration_list
+    except Exception as e:
+        logger.error(e)
+
+
 def lambda_handler(event, context):
     logger.info(json.dumps(event))
     try:
